a9456a1f58be2231b456ef8da074bf5108bf5f64
Bleeky <hausser.quentin@gmail.com>
2014-10-07 13:43:36 +0200

    Seed Corrections

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -19,7 +19,55 @@ class PictureTableSeeder extends Seeder {
 
         Picture::create(array(
             'title' => 'Test 1',
-            'html'  =>  "<p style='text-align: center;' class='title'><em><span style='font-family: 'Josefin Sans'; font-size: 60px;'>Article Prototype</span></em></p><p><img class='fr-fin' data-fr-image-preview='false' alt='Image title' src='http://localhost:8000/ressources/pictures/large/holder.png' width='530'></p><p class='content'><span style='font-family: 'Open Sans'; font-size: 21px;'>This is a example content for your image. Please write your own here !</span></p>",
+            'html'  =>  "<p style=\"text-align: center;\" class=\"title\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/pictures/large/holder.png\" width=\"530\"></p><p class=\"content\"><span style=\"font-family: 'Open Sans'; font-size: 21px;\">This is a example content for your image. Please write your own here !</span></p>",
+            'content' =>    "This is a example content for your image. Please write your own here !",
+            'image' => 'holder.png',
+            'status' => '1',
+        ));
+        sleep(1);
+        Picture::create(array(
+            'title' => 'Test 2',
+            'html'  =>  "<p style=\"text-align: center;\" class=\"title\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/pictures/large/holder.png\" width=\"530\"></p><p class=\"content\"><span style=\"font-family: 'Open Sans'; font-size: 21px;\">This is a example content for your image. Please write your own here !</span></p>",
+            'content' =>    "This is a example content for your image. Please write your own here !",
+            'image' => 'holder.png',
+            'status' => '1',
+        ));
+        sleep(1);
+        Picture::create(array(
+            'title' => 'Test 3',
+            'html'  =>  "<p style=\"text-align: center;\" class=\"title\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/pictures/large/holder.png\" width=\"530\"></p><p class=\"content\"><span style=\"font-family: 'Open Sans'; font-size: 21px;\">This is a example content for your image. Please write your own here !</span></p>",
+            'content' =>    "This is a example content for your image. Please write your own here !",
+            'image' => 'holder.png',
+            'status' => '1',
+        ));
+        sleep(1);
+        Picture::create(array(
+            'title' => 'Test 4',
+            'html'  =>  "<p style=\"text-align: center;\" class=\"title\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/pictures/large/holder.png\" width=\"530\"></p><p class=\"content\"><span style=\"font-family: 'Open Sans'; font-size: 21px;\">This is a example content for your image. Please write your own here !</span></p>",
+            'content' =>    "This is a example content for your image. Please write your own here !",
+            'image' => 'holder.png',
+            'status' => '1',
+        ));
+        sleep(1);
+        Picture::create(array(
+            'title' => 'Test 5',
+            'html'  =>  "<p style=\"text-align: center;\" class=\"title\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/pictures/large/holder.png\" width=\"530\"></p><p class=\"content\"><span style=\"font-family: 'Open Sans'; font-size: 21px;\">This is a example content for your image. Please write your own here !</span></p>",
+            'content' =>    "This is a example content for your image. Please write your own here !",
+            'image' => 'holder.png',
+            'status' => '1',
+        ));
+        sleep(1);
+        Picture::create(array(
+            'title' => 'Test 6',
+            'html'  =>  "<p style=\"text-align: center;\" class=\"title\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/pictures/large/holder.png\" width=\"530\"></p><p class=\"content\"><span style=\"font-family: 'Open Sans'; font-size: 21px;\">This is a example content for your image. Please write your own here !</span></p>",
+            'content' =>    "This is a example content for your image. Please write your own here !",
+            'image' => 'holder.png',
+            'status' => '1',
+        ));
+        sleep(1);
+        Picture::create(array(
+            'title' => 'Test 7',
+            'html'  =>  "<p style=\"text-align: center;\" class=\"title\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/pictures/large/holder.png\" width=\"530\"></p><p class=\"content\"><span style=\"font-family: 'Open Sans'; font-size: 21px;\">This is a example content for your image. Please write your own here !</span></p>",
             'content' =>    "This is a example content for your image. Please write your own here !",
             'image' => 'holder.png',
             'status' => '1',
@@ -36,7 +84,39 @@ class ArticleTableSeeder extends Seeder {
 
         Article::create(array(
             'title'         => 'Test 1',
-            'content'   =>  "<p class='title' style='text-align: center;'><em><span style='font-family: 'Josefin Sans'; font-size: 60px;'>Article Prototype</span></em></p><p><img class='fr-fin' data-fr-image-preview='false' alt='Image title' src='http://localhost:8000/ressources/articles/holder.png' width='683'></p><p class='introduction'><span style='font-family: 'Open Sans'; font-size: 20px;'>This is a small article introduction. You should write your own here. Have fun !</span></p>",
+            'content'   =>  "<p class=\"title\" style=\"text-align: center;\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/articles/holder.png\" width=\"683\"></p><p class=\"introduction\"><span style=\"font-family: 'Open Sans'; font-size: 20px;\">This is a small article introduction. You should write your own here. Have fun !</span></p>",
+            'introduction'  => "This is a small article introduction. You should write your own here. Have fun !",
+            'image'         => 'http://localhost:8000/ressources/articles/holder.png',
+            'status'         => '1',
+        ));
+        sleep(1);
+        Article::create(array(
+            'title'         => 'Test 2',
+            'content'   =>  "<p class=\"title\" style=\"text-align: center;\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/articles/holder.png\" width=\"683\"></p><p class=\"introduction\"><span style=\"font-family: 'Open Sans'; font-size: 20px;\">This is a small article introduction. You should write your own here. Have fun !</span></p>",
+            'introduction'  => "This is a small article introduction. You should write your own here. Have fun !",
+            'image'         => 'http://localhost:8000/ressources/articles/holder.png',
+            'status'         => '1',
+        ));
+        sleep(1);
+        Article::create(array(
+            'title'         => 'Test 3',
+            'content'   =>  "<p class=\"title\" style=\"text-align: center;\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/articles/holder.png\" width=\"683\"></p><p class=\"introduction\"><span style=\"font-family: 'Open Sans'; font-size: 20px;\">This is a small article introduction. You should write your own here. Have fun !</span></p>",
+            'introduction'  => "This is a small article introduction. You should write your own here. Have fun !",
+            'image'         => 'http://localhost:8000/ressources/articles/holder.png',
+            'status'         => '1',
+        ));
+        sleep(1);
+        Article::create(array(
+            'title'         => 'Test 4',
+            'content'   =>  "<p class=\"title\" style=\"text-align: center;\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/articles/holder.png\" width=\"683\"></p><p class=\"introduction\"><span style=\"font-family: 'Open Sans'; font-size: 20px;\">This is a small article introduction. You should write your own here. Have fun !</span></p>",
+            'introduction'  => "This is a small article introduction. You should write your own here. Have fun !",
+            'image'         => 'http://localhost:8000/ressources/articles/holder.png',
+            'status'         => '1',
+        ));
+        sleep(1);
+        Article::create(array(
+            'title'         => 'Test 5',
+            'content'   =>  "<p class=\"title\" style=\"text-align: center;\"><em><span style=\"font-family: 'Josefin Sans'; font-size: 60px;\">Article Prototype</span></em></p><p><img class=\"fr-fin\" data-fr-image-preview=\"false\" alt=\"Image title\" src=\"http://localhost:8000/ressources/articles/holder.png\" width=\"683\"></p><p class=\"introduction\"><span style=\"font-family: 'Open Sans'; font-size: 20px;\">This is a small article introduction. You should write your own here. Have fun !</span></p>",
             'introduction'  => "This is a small article introduction. You should write your own here. Have fun !",
             'image'         => 'http://localhost:8000/ressources/articles/holder.png',
             'status'         => '1',
