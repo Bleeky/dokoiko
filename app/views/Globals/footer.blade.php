21bcc4f5857058a896c5db2a16826b4cc6b6502c
Quentin <hausser.quentin@gmail.com>
2014-10-28 16:05:09 +0100

    Preparing sass and cleaning css

--- app/views/Globals/footer.blade.php
+++ app/views/Globals/footer.blade.php
@@ -1,18 +1,14 @@
 <div class="footer">
-	<div class="navbar navbar-inverse navbar-static-top copyrights transparentcolor">
-	    <div class="wrap">
+    <div class="wrap transparentcolor">
 	    <div class="socials">
-	        <a target="_blank" class="tooltipdefault" title="Twitter" href="lol"><i class="fa fa-twitter fa-2x"></i></a>
-            <a target="_blank" class="tooltipdefault" title="Google+" href="lol"><i class="fa fa-google-plus fa-2x"></i></a>
-            <a target="_blank" class="tooltipdefault" title="Facebook" href="lol"><i class="fa fa-facebook fa-2x"></i></a>
+	        <a target="_blank" class="tooltipdefault footer-hover" title="Twitter" href="lol"><i class="fa fa-twitter fa-2x"></i></a>
+            <a target="_blank" class="tooltipdefault footer-hover" title="Google+" href="lol"><i class="fa fa-google-plus fa-2x"></i></a>
+            <a target="_blank" class="tooltipdefault footer-hover" title="Facebook" href="lol"><i class="fa fa-facebook fa-2x"></i></a>
         </div>
         <hr style="color: white;" width="200px;">
-        <copyrights>
     		© Copyright 2014 Dokoiko. All Rights Reserved.<br>
-    		Illustrations by <a target="_blank" class="ganitzsh" href="http://ganitzsh.deviantart.com">Ganitzsh</a>.
-    	</copyrights>
-	    </div>
-	</div>
+    		Illustrations by <a target="_blank" class="footer-hover" href="http://ganitzsh.deviantart.com">Ganitzsh</a>. Yeah.
+    </div>
 </div>
 
 {{ HTML::script('js/bootstrap.min.js') }}
