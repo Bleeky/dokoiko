497c7612d101cdfe8c6fc569c7700dc1e8ff6120
Quentin <hausser.quentin@gmail.com>
2014-12-31 15:21:13 +0100

    Cleaner readme

--- app/models/Article.php
+++ app/models/Article.php
@@ -1,14 +1,16 @@
 <?php
 
-class Article extends Eloquent
-{
+class Article extends Eloquent {
+
 	public $timestamps = false;
-    public $incrementing = false;
+	public $incrementing = false;
 	protected $table = 'articles';
 	protected $fillable = array('title', 'introduction', 'content', 'image', 'status', 'date');
 
-	public function getDate() {
-		setlocale (LC_TIME, 'fr_FR');
+	public function getDate()
+	{
+		setlocale(LC_TIME, 'fr_FR');
+
 		return ucfirst(strftime("%A %d ", strtotime($this->date))) . ucfirst(strftime("%B %Y", strtotime($this->date)));
 	}
 }
